de505eb54fba83b3b5347b666a699835ac66b927
Zaahy <zaahy10@gmail.com>
2026-03-25 08:52:16 -0400

    Task 2 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,8 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const ISBN = req.params.isbn;
+    res.send(books[ISBN])
  });
   
 // Get book details based on author
