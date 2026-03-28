590643e3db315f6d796eb3233084595ce12216ad
Zaahy <zaahy10@gmail.com>
2026-03-28 10:39:22 -0400

    Task 5 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -49,8 +49,13 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    if(books[isbn]){
+        return res.status(200).send(JSON.stringify(books[isbn].reviews,null,4));
+    }
+    else{
+        return res.status(404).send("No book found with ISBN "+isbn);
+    }
 });
 
 module.exports.general = public_users;
